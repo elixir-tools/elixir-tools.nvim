db4f24252b91f1d8140ad2133bf192bedfff87ec
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-04-11 23:41:51 -0400

    feat: credo lsp

---
+++ lua/elixir/credo/init.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+function M.setup(opts)
+  local credo = vim.api.nvim_create_augroup("elixir-tools.credo", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    group = credo,
+    pattern = { "elixir" },
+    callback = function()
+      local file =
+        vim.fs.find({ "mix.exs" }, { upward = true, path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) })[1]
+
+      local function read_file(path)
+        local f = io.open(path, "rb")
+        if not f then
+          return nil
+        end
+        local content = f:read("*a")
+        f:close()
+        return content
+      end
+
+      if file and not read_file(file):find("{:credo, ") then
+        file = nil
+      end
+
+      vim.lsp.start {
+        name = "Credo",
+        cmd = { "mix", "credo.lsp", "--stdio" },
+        settings = {},
+        root_dir = vim.fs.dirname(file),
+        on_attach = opts.on_attach or function() end,
+      }
+    end,
+  })
+end
+
+return M
--- lua/elixir/init.lua
+++ lua/elixir/init.lua
@@ -1,4 +1,5 @@
 local elixirls = require("elixir.elixirls")
+local credo = require("elixir.credo")
 local mix = require("elixir.mix")
 local projectionist = require("elixir.projectionist")
 
@@ -12,7 +13,10 @@ M.elixirls.open_output_panel = elixirls.open_output_panel
 function M.setup(opts)
   mix.setup()
   projectionist.setup()
-  elixirls.setup(opts["elixirls"] or {})
+  elixirls.setup(opts.elixirls or {})
+  if opts.credo then
+    credo.setup(opts.credo)
+  end
 end
 
 return M
