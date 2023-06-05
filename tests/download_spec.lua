571509db59f7ac6c44c90c79d0a00e562548ca68
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-06-04 20:16:30 -0400

    fixup! ci: stylua-action

--- tests/download_spec.lua
+++ tests/download_spec.lua
@@ -2,49 +2,50 @@ local Path = require("plenary.path")
 local eq = assert.are.same
 local Download = require("elixir.elixirls.download")
 vim.notify = function(thing)
-	io.stdout:write(thing .. "\n")
+  io.stdout:write(thing .. "\n")
 end
 
 describe("download", function()
-	before_each(function()
-		vim.fn.system([[rm -rf tmp/downloads]])
+  before_each(function()
+    vim.fn.system([[rm -rf tmp/downloads]])
 
-		assert(not Path:new("tmp/downloads"):exists(), "tmp/downloads was not deleted")
-	end)
+    assert(not Path:new("tmp/downloads"):exists(), "tmp/downloads was not deleted")
+  end)
 
-	it("can git clone HEAD of the source code", function()
-		local download_dir = "tmp/downloads"
+  it("can git clone HEAD of the source code", function()
+    local download_dir = "tmp/downloads"
 
-		local result = Download.clone(download_dir, { repo = "elixir-lsp/elixir-ls", ref = "HEAD" })
+    local result = Download.clone(download_dir, { repo = "elixir-lsp/elixir-ls", ref = "HEAD" })
 
-		eq("elixir-lsp/elixir-ls/HEAD", result)
-		assert.True(Path:new(download_dir, "elixir-lsp/elixir-ls/HEAD", "mix.exs"):exists())
-	end)
+    eq("elixir-lsp/elixir-ls/HEAD", result)
+    assert.True(Path:new(download_dir, "elixir-lsp/elixir-ls/HEAD", "mix.exs"):exists())
+  end)
 
-	it("can clone from a different repository", function()
-		local download_dir = "tmp/downloads"
+  it("can clone from a different repository", function()
+    local download_dir = "tmp/downloads"
 
-		local result = Download.clone(download_dir, { repo = "mhanberg/elixir-ls", ref = "HEAD" })
+    local result = Download.clone(download_dir, { repo = "mhanberg/elixir-ls", ref = "HEAD" })
 
-		eq("mhanberg/elixir-ls/HEAD", result)
-		assert.True(Path:new(download_dir, "mhanberg/elixir-ls/HEAD", "mix.exs"):exists())
-	end)
+    eq("mhanberg/elixir-ls/HEAD", result)
+    assert.True(Path:new(download_dir, "mhanberg/elixir-ls/HEAD", "mix.exs"):exists())
+  end)
 
-	it("can checkout a different branch", function()
-		local download_dir = "tmp/downloads"
+  it("can checkout a different branch", function()
+    local download_dir = "tmp/downloads"
 
-		local result = Download.clone(download_dir, { repo = "mhanberg/elixir-ls", ref = "mh/all-workspace-symbols" })
+    local result =
+      Download.clone(download_dir, { repo = "mhanberg/elixir-ls", ref = "mh/all-workspace-symbols" })
 
-		eq("mhanberg/elixir-ls/mh_all-workspace-symbols", result)
-		assert.True(Path:new(download_dir, "mhanberg/elixir-ls/mh_all-workspace-symbols", "mix.exs"):exists())
-	end)
+    eq("mhanberg/elixir-ls/mh_all-workspace-symbols", result)
+    assert.True(Path:new(download_dir, "mhanberg/elixir-ls/mh_all-workspace-symbols", "mix.exs"):exists())
+  end)
 
-	it("can checkout a different tag", function()
-		local download_dir = "tmp/downloads"
+  it("can checkout a different tag", function()
+    local download_dir = "tmp/downloads"
 
-		local result = Download.clone(download_dir, { repo = "elixir-lsp/elixir-ls", ref = "tags/v0.14.6" })
+    local result = Download.clone(download_dir, { repo = "elixir-lsp/elixir-ls", ref = "tags/v0.14.6" })
 
-		eq("elixir-lsp/elixir-ls/tags_v0.14.6", result)
-		assert.True(Path:new(download_dir, "elixir-lsp/elixir-ls/tags_v0.14.6", "mix.exs"):exists())
-	end)
+    eq("elixir-lsp/elixir-ls/tags_v0.14.6", result)
+    assert.True(Path:new(download_dir, "elixir-lsp/elixir-ls/tags_v0.14.6", "mix.exs"):exists())
+  end)
 end)
--- tests/elixirls_spec.lua
+++ tests/elixirls_spec.lua
@@ -2,37 +2,37 @@ local shell = vim.fn.system
 local elixirls = require("elixir.elixirls")
 
 describe("elixir", function()
-	describe("command", function()
-		after_each(function()
-			shell("rm -rf tmp/fake_install")
-		end)
+  describe("command", function()
+    after_each(function()
+      shell("rm -rf tmp/fake_install")
+    end)
 
-		it("returns false when it's not installed", function()
-			local install_path = "tmp/fake_install"
-			local result = elixirls.command({
-				path = install_path,
-				repo = "foo/biz",
-				ref = "bar/baz",
-				versions = "foobarbaz",
-			})
+    it("returns false when it's not installed", function()
+      local install_path = "tmp/fake_install"
+      local result = elixirls.command {
+        path = install_path,
+        repo = "foo/biz",
+        ref = "bar/baz",
+        versions = "foobarbaz",
+      }
 
-			assert.are.same("tmp/fake_install/foo/biz/bar_baz/foobarbaz/language_server.sh", tostring(result))
-			assert.False(result:exists())
-		end)
+      assert.are.same("tmp/fake_install/foo/biz/bar_baz/foobarbaz/language_server.sh", tostring(result))
+      assert.False(result:exists())
+    end)
 
-		it("returns true when it is installed", function()
-			local install_path = "tmp/fake_install"
-			shell("mkdir -p tmp/fake_install/foo/biz/bar_baz/foobarbaz")
-			shell("touch tmp/fake_install/foo/biz/bar_baz/foobarbaz/language_server.sh")
+    it("returns true when it is installed", function()
+      local install_path = "tmp/fake_install"
+      shell("mkdir -p tmp/fake_install/foo/biz/bar_baz/foobarbaz")
+      shell("touch tmp/fake_install/foo/biz/bar_baz/foobarbaz/language_server.sh")
 
-			local result = elixirls.command({
-				path = install_path,
-				repo = "foo/biz",
-				ref = "bar/baz",
-				versions = "foobarbaz",
-			})
+      local result = elixirls.command {
+        path = install_path,
+        repo = "foo/biz",
+        ref = "bar/baz",
+        versions = "foobarbaz",
+      }
 
-			assert.True(result:exists())
-		end)
-	end)
+      assert.True(result:exists())
+    end)
+  end)
 end)
