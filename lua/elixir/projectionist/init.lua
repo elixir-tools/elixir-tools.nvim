ce4921dc3b864c6bbc263ffd79bcc924acf5bf13
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-06-11 00:10:49 -0400

    fix(projectionist): don't append live to file and module name

--- lua/elixir/projectionist/init.lua
+++ lua/elixir/projectionist/init.lua
@@ -134,20 +134,20 @@ local config = {
         "end",
       },
     },
-    ["lib/**/live/*_live.ex"] = {
+    ["lib/**/live/*.ex"] = {
       type = "liveview",
-      alternate = "test/{dirname}/live/{basename}_live_test.exs",
+      alternate = "test/{dirname}/live/{basename}_test.exs",
       template = {
-        "defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}Live do",
+        "defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize} do",
         "  use {dirname|camelcase|capitalize}, :live_view",
         "end",
       },
     },
-    ["test/**/live/*_live_test.exs"] = {
+    ["test/**/live/*_test.exs"] = {
       type = "test",
-      alternate = "lib/{dirname}/live/{basename}_live.ex",
+      alternate = "lib/{dirname}/live/{basename}.ex",
       template = {
-        "defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}LiveTest do",
+        "defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}Test do",
         "  use {dirname|camelcase|capitalize}.ConnCase",
         "",
         "  import Phoenix.LiveViewTest",
