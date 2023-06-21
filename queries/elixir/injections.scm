44645316f987d78d1afe6e6f8ac46839bb5a2bbc
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-06-21 08:11:32 -0400

    feat(elixir,treesitter): highlight arg to Repo.query! as SQL

--- queries/elixir/injections.scm
+++ queries/elixir/injections.scm
@@ -6,6 +6,8 @@
     (string
       (quoted_content) @sql)))
 
+(call (dot left: (alias) @_alias (#eq? @_alias "Repo") right: (identifier) @_identifier (#eq? @_identifier "query!")) (arguments (string (quoted_content) @sql))) @foo
+
 ((call
    target: (dot
              left: (alias) @_mod (#eq? @_mod "EEx")
