284c47118237775006be7930d2ab9f22775cf1aa
André Luiz <andre.paes@contaswap.com>
2023-09-04 11:37:05 -0300

    add mix.exs to credo and nextls init.lua

--- lua/elixir/credo/init.lua
+++ lua/elixir/credo/init.lua
@@ -11,7 +11,7 @@ function M.setup(opts)
     group = credo,
     pattern = { "elixir" },
     callback = function()
-      local matches = vim.fs.find({ "mix.lock" }, {
+      local matches = vim.fs.find({ "mix.lock", "mix.exs" }, {
         stop = vim.uv.os_homedir(),
         upward = true,
         path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
